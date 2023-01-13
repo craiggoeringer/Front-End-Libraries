3344272e4c969351e04954aa05a9043a8e67f22d
craiggoeringer <106003605+craiggoeringer@users.noreply.github.com>
2023-01-13 17:23:35 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Markdown Previewer
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/creglesisathing/pen/jOzmrYV](https://codepen.io/creglesisathing/pen/jOzmrYV).
+
---
+++ script.js
@@ -0,0 +1,63 @@
+let editorName = document.querySelector('#editor');
+let previewName = document.querySelector('#preview');
+editorName.addEventListener('input', editorFunction);
+function editorFunction() {
+
+  previewName.innerHTML = marked(editorName.value, { breaks: true });
+
+}
+
+
+window.onload = onLoading;
+
+function onLoading() {
+
+  let defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/umyytc)
+`;
+
+  editorName.value = defaultText;
+  previewName.innerHTML = marked(defaultText, { breaks: true });
+
+}
